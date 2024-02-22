af8a51a382768e0fbd43553b83d03dd9f758580f
MichaelRay2 <michaelray2809@yahoo.com>
2024-02-22 10:21:18 +0000

    Added a variable with an array of questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+const questions = [
+    {
+        question: "Which is the capital city of Spain?",
+        answers: [
+            { text: "Barcelona", correct: false},
+            { text: "Madrid", correct: true},
+            { text: "Sevilla", correct: false}
+            { text: "Valencia", correct: false}
+        ]
+    },
+    { 
+        question: "Which is the capital city of Australia?",
+        answers: [
+            { text: "Sydney", correct: false},
+            { text: "Melbourne", correct: false},
+            { text: "Brisbane", correct: false},
+            { text: "Canberra", correct: true},
+        ]
+    },
+    {
+        question: "Which is the capital city of Nigeria?", 
+        answers: [
+            { text: "Abuja", correct: true},
+            { text: "Accra", correct: false},
+            { text: "Durban", correct: false},
+            { text: "Lagos", correct: false},
+        ]
+    },
+    {
+        question: "Which is the capital city of Canada?",
+        answers: [
+            { text: "Toronto", correct: false},
+            { text: "Montreal", correct: false},
+            { text: "Ottawa", correct: true},
+            { text: "Vancouver", correct: false},
+        ]
+    },
+    {
+        question: "Which is the capital city of China?",
+        answers: [
+            { text: "Beijing", correct: true},
+            { text: "Shanghai", correct: false},
+            { text: "Wuhan", correct: false},
+            { text: "Hong Kong", correct: false},
+        ]
+    },
+    {
+        question: "Which is the capital city of India?",
+        answers: [
+            { text: "Beijing", correct: true},
+            { text: "Shanghai", correct: false},
+            { text: "Wuhan", correct: false},
+            { text: "Hong Kong", correct: false},
+        ]
+    },
+    {
+        question: "Which is the capital city of Brazil?",
+        answers: [
+            { text: "Sao Paulo", correct: true},
+            { text: "Brasilia", correct: false},
+            { text: "Rio de Janeiro", correct: false},
+            { text: "Belo Horizonte", correct: false},
+        ]
+    },
+    {
+        question: "Which is the capital city of Switzerland?",
+        answers: [
+            { text: "Lucerne", correct: false},
+            { text: "Basel", correct: false},
+            { text: "Zurich", correct: false},
+            { text: "Bern", correct: true},
+        ]
+    } ]
+
